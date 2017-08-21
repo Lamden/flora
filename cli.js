32cb07bcee02443d2d7737bfed06d07794e2633c
StuartFarmer <email@stuartfarmer.me>
2017-08-20 22:52:15 -0400

    cli args instead of an app

--- cli.js
+++ cli.js
@@ -1,7 +1,13 @@
 const vorpal = require('vorpal')();
+const sync = require('sync');
+const IPFS = require('ipfs-daemon/src/ipfs-node-daemon')
+const OrbitDB = require('orbit-db')
+
+const ipfs = new IPFS()
 
 vorpal
   .command('install <packageName>')
+  .description('Attempts to install a Solidity smart contract from the IPFS package database.')
   .action(function (args, callback) {
     let str = args.packageName;
     this.log(str);
@@ -10,12 +16,15 @@ vorpal
 
 vorpal
   .command('upload <fileName>')
+  .description('Uploads a new package to the IPFS package database.')
   .action(function (args, callback) {
     let str = args.fileName;
     this.log(str);
     callback();
   });
 
-vorpal
-  .delimiter('flora$')
-  .show();
+// vorpal
+//   .delimiter('flora$')
+//   .show();
+
+vorpal.parse(process.argv);
