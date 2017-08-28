47606519c893ed0636a968b41ac17e7e91bb00c2
StuartFarmer <email@stuartfarmer.me>
2017-08-28 19:57:33 -0400

    class created

--- flora.py
+++ flora.py
@@ -1,46 +1,17 @@
-import sqlite3
-import ipfsapi
 import click
-import subprocess
-import time
-import pickle
+from ipfs import IPFS
 
 # where's the pickle?
 MASTER_DB_HASH = 0
 
-# timeout for daemon launch
-TIMEOUT = 2
-
-def connect():
-	print('Connecting to IPFS...')
-	return ipfsapi.connect('127.0.0.1', 5001)
-
-api = None
-proc = None
-try:
-	api = connect()
-except Exception as e:
-	# start daemon
-	print('IPFS Daemon not running, starting now...')
-	proc = subprocess.Popen(['ipfs', 'daemon'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-	time.sleep(TIMEOUT)
-	try:
-		api = connect()
-	except:
-		raise 'Timeout error.'
-
-print('Connected to IPFS.')
-
-@click.command()
+@click.group()
 def cli():
 	"""This script showcases different terminal UI helpers in Click."""
-	pass
+	print(IPFS)
+	print('howdy')
 
-# @cli.command()
-# def install():
-# 	click.echo('package')
-
-if proc != None:
-	print('Killing IPFS Daemon...')
-	proc = subprocess.Popen(['killall', 'ipfs'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-	print('Done.')
+@cli.command()
+def install():
+	click.echo('package')
+	#if proc != None:
+	#IPFS.kill()
---
+++ ipfs.py
@@ -0,0 +1,48 @@
+import sqlite3
+import ipfsapi
+import subprocess
+import time
+import pickle
+
+TIMEOUT = 2
+RETRIES = 3
+
+class IPFS(object):
+	def __init__(self):
+		self.api = None
+		self.proc = None
+		try:
+			api = connect()
+		except Exception as e:
+			# if not, start daemon
+			print('IPFS Daemon not running, starting now...')
+			proc = subprocess.Popen(['ipfs', 'daemon'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+			retry = 0
+			while retry > RETRIES:
+				# wait a second to let it start
+				time.sleep(TIMEOUT)
+				try:
+					api = connect()
+					break
+				except:
+					print('Timed out...')
+					retry += 1
+				# repeat connect attempts until retries are satisfied
+			if retry == RETRIES:
+				# throw error if it went through all the retries
+				raise Error('Could not connect...')
+
+		# otherwise continue
+		print('Connected to IPFS.')
+
+	def connect(self):
+		print('Connecting to IPFS...')
+		return ipfsapi.connect('127.0.0.1', 5001)
+
+	def kill(self):
+		if self.proc != None:
+			print('Killing IPFS Daemon...')
+			subprocess.Popen(['killall', 'ipfs'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+			print('Done.')
+
+IPFS = IPFS()
