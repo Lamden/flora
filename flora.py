994a9c401a9d7db05b4604ab21ad4cf2b2acf03b
jmunsch <james.a.munsch@gmail.com>
2017-09-03 17:39:52 -0700

    flora.py

--- flora.py
+++ flora.py
@@ -15,3 +15,16 @@ def install():
 	click.echo('package')
 	#if proc != None:
 	#IPFS.kill()
+
+@cli.command()
+def push():
+    click.echo('push')
+    # TODO
+    # xz --robot --info-memory
+    # XZ_DEFAULTS
+    # XZ_OPT
+    # XZ_OPT=${XZ_OPT-"-7e"}
+    # xz -cvfJ -7 git.repo.tar.xz this_git_repo
+    # ipfs add
+    # ipns route
+    # return hash and address info
