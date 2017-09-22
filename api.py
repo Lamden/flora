edafcfa3dd08e90806352948e7c6de6ccb223b4d
StuartFarmer <email@stuartfarmer.me>
2017-09-22 13:03:23 -0400

    merge problems

--- api.py
+++ api.py
@@ -99,11 +99,8 @@ class PackageRegistry(Resource):
 			
 			# create and store a new secret
 			secret = random_string(53)
-
-			# sign with server public key in memory and decrypt later with the private key
-			server_signed_secret = rsa.encrypt(secret.encode('utf8'), KEY[0])
-			query = conn.execute("UPDATE names SET secret=:secret WHERE name=:name", {'secret' : str(server_signed_secret), 'name' : owner})
-
+			query = conn.execute("UPDATE names SET secret='{}' WHERE name='{}'".format(secret, owner))
+			
 			# sign and send secret
 			user_signed_secret = rsa.encrypt(secret.encode('utf8'), user_public_key)
 			return success_payload(str(user_signed_secret), 'Package available to register.')
@@ -115,11 +112,11 @@ class PackageRegistry(Resource):
 		owner = request.form['owner']
 		package = request.form['package']
 		data = request.form['data']
+		print(data)
 		# get the secret from the db
 		conn = engine.connect()
 		query = conn.execute("SELECT secret FROM names WHERE name='{}'".format(owner)).fetchone()
-		if query == None:
-				return error_payload('Secret not found.')
+		print(query[0])
 
 		# data is a python tuple of the templated solidity at index 0 and an example payload at index 1
 		# compilation of this code should return true
@@ -166,4 +163,5 @@ api.add_resource(Authorization, '/auth')
 if __name__ == '__main__':
 	(pub, priv) = rsa.newkeys(512)
 	KEY = (pub, priv)
+	print(KEY)
 	app.run(debug=True)
